617778efa0a24991067258c60da0141bee575846
Peter Schallot <pschallot@medonesystems.com>
2022-07-07 15:35:14 -0400

    Zaphod's just this guy, you know?

---
+++ CQMParse/MeasureParse.cs
@@ -0,0 +1,43 @@
+﻿using HtmlAgilityPack;
+using System.Net;
+
+namespace CQMParse;
+
+public class MeasureParse
+{
+    private static string GetHtmlFromUrl(string url)
+    {
+        using WebClient web1 = new WebClient() ;
+        string data = web1.DownloadString(url);
+        return data;
+    }
+    
+    public static void Parse(string measureUrl)
+    {
+
+        var filePath = @"C:\code\CQMParser\CQMParse\Antithrombotic Therapy By End of Hospital Day 2 10.6.000.html";
+        var doc = new HtmlDocument();
+        doc.Load(filePath);
+        
+        // var web = new HtmlWeb();
+        // var doc = web.Load(measureUrl);
+
+        var tableOfContentsNode = doc.DocumentNode.SelectSingleNode("//a[@name='toc']");
+        if (tableOfContentsNode == null)
+        {
+            Console.WriteLine("No Table Of Contents was found.");
+            return;
+        }
+
+        var tocContents = tableOfContentsNode.NextSibling;
+        foreach (var tocEntry in tocContents.SelectNodes("//a"))
+        {
+            Console.WriteLine(tocEntry.InnerHtml + " => " + tocEntry.Attributes["name"].Value);
+        }
+        
+        
+        
+        Console.WriteLine(tableOfContentsNode.InnerHtml);
+        
+    }
+}
---
+++ CQMParse/Program.cs
@@ -0,0 +1,3 @@
+﻿using CQMParse;
+
+MeasureParse.Parse("https://ecqi.healthit.gov/sites/default/files/ecqm/measures/CMS72v10.html");
