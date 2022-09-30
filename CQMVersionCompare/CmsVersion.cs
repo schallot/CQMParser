736a41d0c1d3853ef91871a6e5e43977b1d043be
Pete Schallot <pschallot@medonesystems.com>
2022-09-30 01:14:38 -0400

    Comparison reports - changes between last two versions

---
+++ CQMVersionCompare/CmsVersion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQMVersionCompare
+{
+    public class CmsVersion
+    {
+        public int CmsNumber { get; }
+        public int Version { get; }
+        public string FilePath { get; }
+
+        public static string GetComparisonFileName(CmsVersion lhs, CmsVersion rhs)
+        {
+            return $"CMS{lhs.CmsNumber}Comparison_{lhs.Version}-{rhs.Version}.html";
+        }
+
+        public CmsVersion(string filePath)
+        {
+            FilePath = filePath;
+            var name = Path.GetFileNameWithoutExtension(filePath);
+            name = name.Replace("CMS", "");
+            var split = name.Split("v");
+            CmsNumber = int.Parse(split[0]);
+            Version = int.Parse(split[1]);
+        }
+    }
+}
---
+++ CQMVersionCompare/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using CQMVersionCompare;
+
+var inputDir = @"C:\code\CQMParser\CompiledMeasures";
+var outputDir = Path.Combine(inputDir, "ComparisonReports");
+if (!Directory.Exists(outputDir))
+{
+    Directory.CreateDirectory(outputDir);
+}
+var groups = Directory.GetFiles(inputDir, "*.txt")
+    .Select(x=>new CmsVersion(x))
+    .GroupBy(x=>x.CmsNumber);
+
+var winMerge = @"C:\Program Files\WinMerge\WinMergeU.exe";
+
+foreach (var g in groups)
+{
+    var items = g.OrderByDescending(x => x.Version).ToArray();
+    if (items.Length > 1)
+    {
+        var rhs = items[0];
+        var lhs = items[1];
+        var reportName = CmsVersion.GetComparisonFileName(lhs, rhs);
+        var reportPath = Path.Combine(outputDir, reportName);
+        var si = new ProcessStartInfo(winMerge, $"\"{lhs.FilePath}\" \"{rhs.FilePath}\" -minimize -noninteractive -ignorews -ignorecodepage -ignoreeol -ignoreblanklines -u -or \"{reportPath}\"");
+        if(File.Exists(reportPath)) File.Delete(reportPath);
+        var p = Process.Start(si);
+        p.WaitForExit();
+    }
+}
+
